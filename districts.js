b002f8b36e1cd675cb94bf81e8b501f905722abd
arifullahafghan143-bit <arifullahafghan143@gmail.com>
2026-09-14 22:19:18 +0430

    Create districts.js

---
+++ districts.js
@@ -0,0 +1,20 @@
+const districts = {
+
+"Kabul":[
+"Bagrami","Chahar Asyab","Deh Sabz","Estalif","Farza",
+"Guldara","Kalakan","Khak Jabar","Musahi","Paghman",
+"Qara Bagh","Shakardara","Surobi","Mir Bacha Kot"
+],
+
+"Paktia":[
+"Gardez","Ahmad Aba","Zurmat","Said Karam","Jaji",
+"Dand Patan","Jani Khel","Zazi Aryob","Laja Mangel",
+"Rohani Baba","Wazi Zadran"
+],
+
+"Ghor":[
+"Firoz Koh","Dolina","Dawlat Yar","Charsada","Lal wa Sarjangal",
+"Pasaband","Saghar","Shahrak","Taywara","Tulak"
+]
+
+};
