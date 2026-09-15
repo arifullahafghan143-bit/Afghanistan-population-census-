47a6903765efc53d9e2861141150bd1e2a7d9957
arifullahafghan143-bit <arifullahafghan143@gmail.com>
2026-09-15 07:13:36 +0430

    Update districts.js

--- districts.js
+++ districts.js
@@ -4,14 +4,19 @@ const DISTRICT_DATA_URL =
 "https://raw.githubusercontent.com/open-admin-data/afghanistan-administrative-divisions/main/data/all-district.json";
 
 fetch(DISTRICT_DATA_URL)
-.then(response => response.json())
+.then(response => {
+  if (!response.ok) {
+    throw new Error("District data could not be loaded");
+  }
+  return response.json();
+})
 .then(data => {
 
   districts = {};
 
   data.forEach(item => {
 
-    const province = item.parent.name.local;
+    const province = item.parent.name.en;
     const district = item.name.local;
 
     if (!districts[province]) {
@@ -20,6 +25,10 @@ fetch(DISTRICT_DATA_URL)
 
     districts[province].push(district);
 
+  });
+
+  Object.keys(districts).forEach(province => {
+    districts[province].sort((a,b) => a.localeCompare(b));
   });
 
   if (typeof loadDistricts === "function") {
